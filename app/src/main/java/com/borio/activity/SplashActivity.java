480ee6cf393c2f1523413d7dd3bc782819923525
Ahmed_Emad <ahmed.e.barakat1994@Gmail.com>
2017-01-10 02:46:05 +0200

    Add AES encryption.

--- app/src/main/java/com/borio/activity/SplashActivity.java
+++ app/src/main/java/com/borio/activity/SplashActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
+import com.borio.AesUtil;
+
 public class SplashActivity extends AppCompatActivity {
 
     private static int SPLASH_TIME_OUT = 1500;
@@ -21,6 +23,21 @@ public class SplashActivity extends AppCompatActivity {
                 finish();
             }
         }, SPLASH_TIME_OUT);
+
+//        String iv = AesUtil.random(16);
+//        String salt = AesUtil.random(16);
+//        String plainText = "Ahmed";
+//        String passphrase = "emad";
+//        System.out.println("iv: " + iv);
+//        System.out.println("salt: " + salt);
+//
+//        AesUtil aesUtil = new AesUtil();
+//        // Encrypt
+//        String cipherText = aesUtil.encrypt(salt, iv, passphrase, plainText);
+//        System.out.println(cipherText);
+//        // Decrypt
+//        String plainText2 = aesUtil.decrypt(salt, iv, passphrase, cipherText);
+//        System.out.println(plainText2);
     }
 
 }
